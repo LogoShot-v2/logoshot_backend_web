009714fec88de541648f16305329bbdc00e0a08b
林又昕 <sylvey_lin@ana.lu.im.ntu.edu.tw>
2022-10-16 22:54:29 +0800

    login

--- apptest.py
+++ apptest.py
@@ -1,5 +1,7 @@
 from crypt import methods
 import email
+from http.client import HTTPException
+from urllib.error import HTTPError
 from flask import Flask, jsonify, send_file, request, make_response, session
 from flask_cors import CORS
 import os
@@ -14,17 +16,20 @@ from requests import session
 from waitress import serve
 import smtplib
 from tools.token import make_token, parseToken, decode_token
+import cv2
+# import os
+# from yolov5.logo_detect import Yolo_Detect
 
 import pyrebase
 
-config={
-  'apiKey': "AIzaSyA_xMc3DtQQp8GmAb9JkTHNWMcyhFiQrNM",
-  'authDomain': "logoshot-7c6a9.firebaseapp.com",
-  'projectId': "logoshot-7c6a9",
-  'storageBucket': "logoshot-7c6a9.appspot.com",
-  'messagingSenderId': "256049641924",
-  'appId': "1:256049641924:web:8e8a278f6b2186b9340c91",
-  'databaseURL': ''
+config = {
+    'apiKey': "AIzaSyA_xMc3DtQQp8GmAb9JkTHNWMcyhFiQrNM",
+    'authDomain': "logoshot-7c6a9.firebaseapp.com",
+    'projectId': "logoshot-7c6a9",
+    'storageBucket': "logoshot-7c6a9.appspot.com",
+    'messagingSenderId': "256049641924",
+    'appId': "1:256049641924:web:8e8a278f6b2186b9340c91",
+    'databaseURL': ''
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -35,11 +40,12 @@ app = Flask(__name__)
 CORS(app, resources={r"/.*": {"origins": ["*"]}})
 # routes
 imgFolderRoutes = "imagelog/"
-## official email
+# official email
 officialEmail = "ntuim2022@gmail.com"
 officialEmailPassword = "cdmqxdubwtbimvif"
-## ip to out
+# ip to out
 ip = "http://140.112.106.82:8081/"
+
 
 @app.route("/registerVerify", methods=['POST'])
 def registerVerify():
@@ -48,14 +54,17 @@ def registerVerify():
     password = content['password']
     name = content['name']
 
-    content = MIMEMultipart()  #建立MIMEMultipart物件
-    content["subject"] = "LogoShot 註冊驗證"  #郵件標題
-    content["from"] = officialEmail  #寄件者
-    content["to"] = email #收件者
-    jwtEncodedUser = make_token({'name': name, 'email': email, 'password': password})
+    content = MIMEMultipart()  # 建立MIMEMultipart物件
+    content["subject"] = "LogoShot 註冊驗證"  # 郵件標題
+    content["from"] = officialEmail  # 寄件者
+    content["to"] = email  # 收件者
+    jwtEncodedUser = make_token(
+        {'name': name, 'email': email, 'password': password})
     print(jwtEncodedUser)
-    print("點擊以下連結驗證並註冊\n{}register?token={}".format(ip, jwtEncodedUser.decode("utf-8")))
-    content.attach(MIMEText("點擊以下連結驗證並註冊\n{}register?token={}".format(ip, jwtEncodedUser.decode("utf-8"))))  #郵件內容
+    print("點擊以下連結驗證並註冊\n{}register?token={}".format(
+        ip, jwtEncodedUser.decode("utf-8")))
+    content.attach(MIMEText("點擊以下連結驗證並註冊\n{}register?token={}".format(
+        ip, jwtEncodedUser.decode("utf-8"))))  # 郵件內容
 
     with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
         try:
@@ -69,6 +78,7 @@ def registerVerify():
 
     return {"res": {"email": email}}
 
+
 @app.route("/register", methods=['GET'])
 def register():
     print('register')
@@ -76,13 +86,15 @@ def register():
     token = request.args.get('token')
     decodedUser = decode_token(token)
 
-    user = auth.create_user_with_email_and_password(decodedUser['email'], decodedUser['password'])
+    user = auth.create_user_with_email_and_password(
+        decodedUser['email'], decodedUser['password'])
     print(user)
     # doc_Ref = dbFireBase.collection(u'Users').document(decodedUser['name'])
     # doc_Ref.set(decodedUser)
 
     print(decodedUser)
     return {"res": {"status": "complete"}}
+
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -91,11 +103,12 @@ def login():
     password = content['password']
     try:
         user = auth.sign_in_with_email_and_password(email, password)
-        print(user)
-    except Exception as err: 
-        print(err)
-        return "Fail to login"
-    
+        # print(user)
+    except Exception as err:
+        # print(err.__doc__)
+        message = eval(err.args[1])['error']['message']
+        return message, 400
+
     return {"res": {"userId": user['localId']}}
 
 
@@ -108,6 +121,15 @@ def imageSearch():
     indicatorX = request.form["indicatorX"]
     indicatorY = request.form["indicatorY"]
     userId = request.form["userId"]
+
+    # searchKeywords = request.form["searchKeywords"]
+    # isSimSound = eval(request.form["isSimSound"].capitalize())
+    # isSimShape = eval(request.form["isSimShape"].capitalize())
+    # target_classcodes = eval(request.form["target_classcodes"])
+    # target_color = request.form["target_color"]
+    # target_applicant = request.form["target_applicant"]
+    # target_startTime = request.form["target_startTime"]
+    # target_endTime = request.form["target_endTime"]
 
     caseno_list1 = []
     if photo != "null":
@@ -136,6 +158,8 @@ def imageSearch():
         print("Time Spent(Image_Model): {}s".format(
             EndTime2 - startTime2), file=sys.stdout)
 
+    returnVal = esQuery(isImageSearchFilter=True)
+
     # loadImage and metadatalist
     # startTime4 = time.time()
     # base64Image_list, metadata_list = [], []
@@ -152,8 +176,7 @@ def imageSearch():
     #     EndTime - startTime), file=sys.stdout)
 
     return jsonify({
-        'base64Images': [],
-        'metadatas': [],
+        'data': returnVal
     })
 
 
@@ -167,13 +190,18 @@ def textSearch():
     target_applicant = request.form["target_applicant"]
     target_startTime = request.form["target_startTime"]
     target_endTime = request.form["target_endTime"]
+    # target_draft_c = request.form["target_draft_c"]
+    # target_draft_e = request.form["target_draft_e"]
+    # target_draft_j = request.form["target_draft_j"]
+
     print(searchKeywords, isSimSound, isSimShape, target_classcodes,
           target_color, target_applicant, target_startTime, target_endTime)
-    print("Type:", type(target_classcodes))
+    # print("Type:", type(target_classcodes))
     # returnVal = []
-    returnVal = esQuery(searchKeywords, isSimSound, isSimShape, target_classcodes,
-                        target_color, target_applicant, target_startTime, target_endTime)
+    returnVal = esQuery(searchKeywords, False, isSimSound, isSimShape, "", "", "", target_classcodes,
+                        "", target_applicant, target_startTime, target_endTime)
 
+    print(returnVal)
     return jsonify({
         'resultData': returnVal
     })
